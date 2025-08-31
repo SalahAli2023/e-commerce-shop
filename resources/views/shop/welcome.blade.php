a9a947f358a609066275e60db76c8ac39aed35a1
salah <salahalhimyari2023@gmail.com>
2025-09-01 02:27:23 +0300

    feat: Implement MVC for shop welcome page

---
+++ resources/views/shop/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Our Shop</title>
+</head>
+<body>
+    <h1>Welcome to Salah shop!</h1>
+    <p>It is your shop for all what your shopping need.</p>
+</body>
+</html>
