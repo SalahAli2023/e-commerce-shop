fb0c823619dc52db74f70fd47f636f8de1c0457a
salah <salahalhimyari2023@gmail.com>
2025-09-05 00:03:51 +0300

    feat: Implement Read (listing & showing single product) function

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+     // Show all products
+    public function index()
+    {
+        $products = Product::all();
+        return view('shop.products', compact('products'));
+    }
+
+    // Show single product
+    public function show($id)
+    {
+        $product = Product::findOrFail($id);// هذه الدالة تقوم بالبحث عن الايدي المطلوب واذا لم تجده ترجع خطأ 404
+        return view('shop.product-details', compact('product'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StoreController;
 use App\Http\Controllers\ShopController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,10 +15,14 @@ Route::get('/shop', [ShopController::class, 'index'])->name('shop');
 Route::get('/', [StoreController::class, 'index']);
 
 // Products pages
-Route::get('/products', [StoreController::class, 'products'])->name('products');
-Route::get('/product/{id}', [StoreController::class, 'productDetails'])->name('product.details');
+// Route::get('/products', [StoreController::class, 'products'])->name('products');
+// Route::get('/product/{id}', [StoreController::class, 'productDetails'])->name('product.details');
 
 // Other pages
 Route::get('/cart', [StoreController::class, 'cart'])->name('cart');
 Route::get('/about', [StoreController::class, 'about'])->name('about');
 Route::get('/contact', [StoreController::class, 'contact'])->name('contact');
+
+// Products pages
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
